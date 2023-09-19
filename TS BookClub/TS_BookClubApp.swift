9cedc0ddc970cc84130682e0ebc54c50d3bf45a4
Thomas Sillmann <contact@thomassillmann.de>
2023-09-19 11:44:11 +0200

    Initial Commit

---
+++ TS BookClub/TS_BookClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TS_BookClubApp.swift
+//  TS BookClub
+//
+//  Created by Thomas Sillmann on 19.09.23.
+//
+
+import SwiftUI
+
+@main
+struct TS_BookClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
